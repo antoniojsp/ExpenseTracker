ff44841d16f8c9c5189aa06af0196d695b5c1453
antonio Silva <antonios@uoregon.edu>
2025-12-09 13:02:51 -0800

    initial commit

---
+++ expenses/theme.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "@chakra-ui/react";
+
+const theme = defineConfig({
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: true,
+  },
+});
+
+export default theme;
