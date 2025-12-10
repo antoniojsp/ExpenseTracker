7e5abaa9b5b049196e234e248f586d89790e733f
antonio Silva <antonios@uoregon.edu>
2025-12-09 22:55:16 -0800

    woeking 2.8

--- my-app/src/theme.js
+++ my-app/src/theme.js
@@ -1,25 +1,10 @@
 
-import { extendTheme } from "@chakra-ui/react";
+import {extendTheme} from '@chakra-ui/react'
+
 
 const theme = extendTheme({
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: true,
-  },
-  colors: {
-    brand: {
-      50: "#e3f2ff",
-      100: "#b3d4ff",
-      200: "#81ff96ff",
-      300: "#4f97ff",
-      400: "#1d79ff",
-      500: "#e60036ff",
-      600: "#b4008dff",
-      700: "#003482",
-      800: "#002252",
-      900: "#000f21",
-    },
-  },
-});
+    initialColorMode : 'dark',
+    useSystemColorMode : false
+})
 
 export default theme;
